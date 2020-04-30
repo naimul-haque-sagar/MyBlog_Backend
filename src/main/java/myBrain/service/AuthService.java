b85d1b61c4eb4dbd51b9f993d9e3f8bad7cdd919
naimul-haque-sagar <naimulhaquesagar24@gmail.com>
2020-04-30 20:42:23 +0600

    password encoder added

--- src/main/java/myBrain/service/AuthService.java
+++ src/main/java/myBrain/service/AuthService.java
@@ -1,6 +1,7 @@
 package myBrain.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import myBrain.dto.RegisterRequest;
@@ -11,13 +12,19 @@ import myBrain.repository.UserRepository;
 public class AuthService {
 	@Autowired
 	UserRepository userRepository;
-
+	@Autowired
+	PasswordEncoder passEncoder;
+	
 	public void signup(RegisterRequest registerRequest) {
 		User user=new User();
 		user.setUserName(registerRequest.getUsername());
-		user.setPassword(registerRequest.getPassword());
+		user.setPassword(passwordEncoder(registerRequest.getPassword()));
 		user.setEmail(registerRequest.getEmail());
 		userRepository.save(user);
+	}
+
+	private String passwordEncoder(String p) {
+		return passEncoder.encode(p);
 	}
 	
 }
