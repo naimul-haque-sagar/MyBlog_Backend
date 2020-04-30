68e41c758dab7065a43532dd72dfc3978b03fe8d
naimul-haque-sagar <naimulhaquesagar24@gmail.com>
2020-04-30 10:20:24 +0600

    basic structure added

---
+++ src/main/java/myBrain/MyBrainApplication.java
@@ -0,0 +1,13 @@
+package myBrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myBrain/MyBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package myBrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
